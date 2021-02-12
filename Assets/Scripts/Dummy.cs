54778e5339c299b25461d47f42db57032e6a790e
ckhuang98 <ckhuang@ucsc.edu>
2021-02-12 18:14:17 -0500

    Added tutorial level and Enemy Dummy

---
+++ Assets/Scripts/Dummy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    public float currentHealth;
+    public float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(currentHealth <=0){
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.name.Equals("SlashSpriteSheet_0") && timer >= .5)
+        {
+            currentHealth -= collider.transform.parent.parent.GetComponent<PlayerController>().whatIsStrength();
+            timer = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/DummyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyHealth : MonoBehaviour
+{
+    Vector3 localScale;
+    Transform transform;
+
+    public Dummy dummy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = this.GetComponent<Transform>();
+        localScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        localScale.x = dummy.currentHealth;
+        transform.localScale = localScale;
+    }
+}
