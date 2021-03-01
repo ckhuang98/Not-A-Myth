0bc764b0b855b8f36b5dc3d9d75a0eda326024c6
ckhuang98 <ckhuang@ucsc.edu>
2021-02-28 21:04:26 -0800

    Fixed line in UI.cs to allow build && increased boss health

--- Assets/Scripts/Boss 1 Scripts/Boss.cs
+++ Assets/Scripts/Boss 1 Scripts/Boss.cs
@@ -41,7 +41,7 @@ public class Boss : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        healthAmount = 25f;
+        healthAmount = 50f;
         rb = GetComponent<Rigidbody2D>();
 
         healthBar.SetMaxValue(healthAmount);
