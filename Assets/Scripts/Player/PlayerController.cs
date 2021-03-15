08374934966db07b22e1b9dea84588f9eb741ba5
amir-alaj <aalaj@ucsc.edu>
2021-03-14 23:42:36 -0700

    Eel and Fire trail do damage. Triangle and Circle changed

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -414,6 +414,10 @@ public class PlayerController : MonoBehaviour {
 
             // If the player collides with boss shockwave attack, 10 damage
             if (withinAggroColliders.CompareTag("Shockwave")) { TakeDamage(10); }
+
+            if (withinAggroColliders.CompareTag("Eel Tendril")) { TakeDamage(10); }
+
+            if (withinAggroColliders.CompareTag("Fire Ball")) { TakeDamage(5); }
             
 
             /* EXAMPLE for other types of damage
