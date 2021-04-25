9b25517d374230c729e7f2f69b8e4680c4702364
MochaCola <kpb1999@comcast.net>
2021-04-25 05:23:37 -0700

    Decorator Sprite Tweak / Tag Clean Attempt
    Cropped a couple of building images to fix weirdness when their sprites were updated.
    Also tried briefly to find a solution for the decorator that doesn't use tags

--- Assets/Scripts/PCG/AddRoom.cs
+++ Assets/Scripts/PCG/AddRoom.cs
@@ -21,10 +21,32 @@ public class AddRoom : MonoBehaviour
 
     private void Decorate(GameObject room)
     {
+        /*
+        List<GameObject> trees = new List<GameObject>();
+        List<GameObject> buildings = new List<GameObject>();
+
+        int i = 0, j = 0;
+        GameObject[] gameobjs = GetComponents<GameObject>();
+        foreach (GameObject child in gameobjs) {
+            Debug.Log(child.name);
+            if (child.name.Contains("TreeSpawnPoint")) {
+                trees.Insert(i, child);
+                i += 1;
+            }
+            if (child.name.Contains("BuildingSpawnPoint")) {
+                buildings.Insert(j, child);
+                j += 1;
+            }
+        }
+        */
+        
         GameObject[] trees = GameObject.FindGameObjectsWithTag("TreeSpawnPoint");
-        Sprite[] treeSprites = Resources.LoadAll<Sprite>("Trees");
+        // GameObject[] trees = GameObject.Find("Tree/TreeSpawnPoint");
 
         GameObject[] buildings = GameObject.FindGameObjectsWithTag("BuildingSpawnPoint");
+        // GameObject[] buildings = GameObject.Find("BuildingSpawnPoint");
+
+        Sprite[] treeSprites = Resources.LoadAll<Sprite>("Trees");
         Sprite[] buildingSprites = Resources.LoadAll<Sprite>("Buildings");
 
         foreach (GameObject tree in trees) {
