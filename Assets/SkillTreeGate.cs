0ef60f2cfefe1ec695449b3c2cff911e3b27fbe6
Tennessee Phillips Ward <twarior@gmail.com>
2021-05-09 11:16:33 -0500

    [Tutorial Update] Skill Tree Gate Implemented

---
+++ Assets/SkillTreeGate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTreeGate : MonoBehaviour
+{
+
+    public Gate gate;
+    public PlayerStats playerStats;
+    private bool gems = false;
+    private bool gainedSkill = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerStats.currentXp.Value >= 5 && gems == false) {
+            gems = true;
+        }
+        if(playerStats.skillPoints.Value == 1) {
+            gainedSkill = true;
+        }
+        if(gems == true && playerStats.skillPoints.Value == 0 && gainedSkill == true) {
+            gate.unlocked = true;
+        }
+    }
+}
