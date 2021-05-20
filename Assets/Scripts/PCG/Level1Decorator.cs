372415b26b94da49649bb5e5a2123fd327c8793e
Tennessee Phillips Ward <twarior@gmail.com>
2021-05-20 11:27:07 -0500

    [Room Examples] Level 1 Enemies and Tree Spawns Added

---
+++ Assets/Scripts/PCG/Level1Decorator.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Decorator : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        Decorate();
+    }
+
+    private void Decorate() {
+
+        GameObject[] NormalTrees = GameObject.FindGameObjectsWithTag("NormalTreeSpawnPoint");
+        // GameObject[] trees = GameObject.Find("Tree/TreeSpawnPoint");
+
+        GameObject[] IceTrees = GameObject.FindGameObjectsWithTag("IceTreeSpawnPoint");
+        // GameObject[] trees = GameObject.Find("Tree/TreeSpawnPoint");
+
+        GameObject[] SnowTrees = GameObject.FindGameObjectsWithTag("SnowTreeSpawnPoint");
+        // GameObject[] trees = GameObject.Find("Tree/TreeSpawnPoint");
+
+        GameObject[] buildings = GameObject.FindGameObjectsWithTag("BuildingSpawnPoint");
+        // GameObject[] buildings = GameObject.Find("BuildingSpawnPoint");
+
+        Sprite[] NormalTreeSprites = Resources.LoadAll<Sprite>("NormalTrees");
+        Sprite[] IceTreeSprites = Resources.LoadAll<Sprite>("IceTrees");
+        Sprite[] SnowTreeSprites = Resources.LoadAll<Sprite>("SnowTrees");
+
+        Sprite[] buildingSprites = Resources.LoadAll<Sprite>("Buildings");
+
+        foreach (GameObject tree in NormalTrees) {
+            SpriteRenderer renderer = tree.GetComponent<SpriteRenderer>();
+            renderer.sprite = NormalTreeSprites[Random.Range(0, NormalTreeSprites.Length - 1)];
+            renderer.enabled = (Random.Range(1, 11) <= 7);
+        }
+
+        foreach (GameObject tree in IceTrees) {
+            SpriteRenderer renderer = tree.GetComponent<SpriteRenderer>();
+            renderer.sprite = IceTreeSprites[Random.Range(0, IceTreeSprites.Length - 1)];
+            renderer.enabled = (Random.Range(1, 11) <= 7);
+        }
+
+        foreach (GameObject tree in SnowTrees) {
+            SpriteRenderer renderer = tree.GetComponent<SpriteRenderer>();
+            renderer.sprite = SnowTreeSprites[Random.Range(0, SnowTreeSprites.Length - 1)];
+            renderer.enabled = (Random.Range(1, 11) <= 7);
+        }
+
+
+        foreach (GameObject building in buildings) {
+            SpriteRenderer renderer = building.GetComponent<SpriteRenderer>();
+            BoxCollider2D box = building.GetComponent<BoxCollider2D>();
+            renderer.enabled = true;
+            box.enabled = true;
+            renderer.sprite = buildingSprites[Random.Range(0, buildingSprites.Length - 1)];
+            if (Random.Range(1, 11) <= 5) {
+                renderer.enabled = false;
+                box.enabled = false;
+            }
+        }
+
+
+    }
+}
