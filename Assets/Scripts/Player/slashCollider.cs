b6a6f6ea7ff1026f767af91956ea70e74bdd76fc
ckhuang98 <ckhuang@ucsc.edu>
2021-05-24 20:59:01 -0400

    [dev] W9 Build fixes

--- Assets/Scripts/Player/slashCollider.cs
+++ Assets/Scripts/Player/slashCollider.cs
@@ -6,11 +6,15 @@ public class slashCollider : MonoBehaviour
 {
     public PolygonCollider2D hurtBox;
     public PolygonCollider2D extendedHurtBox;
+
+    public CircleCollider2D groundSmash;
     // Start is called before the first frame update
     void Start()
     {
         hurtBox.enabled = false;
         extendedHurtBox.enabled = false;
+        groundSmash.enabled = false;
+
     }
 
     // Update is called once per frame
@@ -26,4 +30,13 @@ public class slashCollider : MonoBehaviour
     public void disableHurtBox(){
         hurtBox.enabled = false;
     }
+
+    public void enableSmashHurtBox(){
+        hurtBox.enabled = true;
+    }
+
+    public void disableSmashHurtBox(){
+        hurtBox.enabled = false;
+    }
+    
 }
